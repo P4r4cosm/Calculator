7b9eed500a8f6491f7e0ac686d80241e70ac2047
Владимир Романенко <romanenko2004@bk.ru>
2024-11-20 23:45:07 +0300

    Numbers realization

--- Form1.cs
+++ Form1.cs
@@ -84,7 +84,7 @@ namespace Calculator
 
         private void button_sqrt_Click(object sender, EventArgs e)
         {
-            result = Calculator.DoFunction(result, Calculator.Function.Sqrt);
+            result = Calculator.DoFunction(Convert.ToInt16(Input.Text), Calculator.Function.Sqrt);
             Input.Text = result.ToString();
         }
 
@@ -101,6 +101,142 @@ namespace Calculator
                 Input.Text = result.ToString();
                 currentOperation = Calculator.Operation.Add; 
             }
+        }
+
+        private void button_7_onclick(object sender, EventArgs e)
+        {
+            if (Input.Text == "0")
+            {
+                Input.Text = "7";
+            }
+            else
+            {
+                Input.Text += "7";
+            }
+        }
+
+        private void button_8_onclick(object sender, EventArgs e)
+        {
+            if (Input.Text == "0")
+            {
+                Input.Text = "8";
+            }
+            else
+            {
+                Input.Text += "8";
+            }
+        }
+
+        private void button_9_onclick(object sender, EventArgs e)
+        {
+            if (Input.Text == "0")
+            {
+                Input.Text = "9";
+            }
+            else
+            {
+                Input.Text += "9";
+            }
+        }
+
+        private void button_4_onclick(object sender, EventArgs e)
+        {
+            if (Input.Text == "0")
+            {
+                Input.Text = "4";
+            }
+            else
+            {
+                Input.Text += "4";
+            }
+        }
+
+        private void button_5_onclick(object sender, EventArgs e)
+        {
+            if (Input.Text == "0")
+            {
+                Input.Text = "5";
+            }
+            else
+            {
+                Input.Text += "5";
+            }
+        }
+
+        private void button_6_onclick(object sender, EventArgs e)
+        {
+            if (Input.Text == "0")
+            {
+                Input.Text = "6";
+            }
+            else
+            {
+                Input.Text += "6";
+            }
+        }
+
+        private void button_1_onclick(object sender, EventArgs e)
+        {
+            if (Input.Text == "0")
+            {
+                Input.Text = "1";
+            }
+            else
+            {
+                Input.Text += "1";
+            }
+        }
+
+        private void button_2_onclick(object sender, EventArgs e)
+        {
+            if (Input.Text == "0")
+            {
+                Input.Text = "2";
+            }
+            else
+            {
+                Input.Text += "2";
+            }
+        }
+
+        private void button_3_onclick(object sender, EventArgs e)
+        {
+            if (Input.Text == "0")
+            {
+                Input.Text = "3";
+            }
+            else
+            {
+                Input.Text += "3";
+            }
+        }
+
+        private void button_00_onclick(object sender, EventArgs e)
+        {
+            if (Input.Text != "0")
+            {
+                Input.Text += "00";
+            }
+            
+        }
+
+        private void button_0_input(object sender, EventArgs e)
+        {
+            if (Input.Text != "0")
+            {
+                Input.Text += "0";
+            }
+            
+        }
+
+        private void button_dot_onclick(object sender, EventArgs e)
+        {
+            Input.Text += ".";
+        }
+
+        private void button_c_onclick(object sender, EventArgs e)
+        {
+            Input.Text = "0";
         }
     }
 }
