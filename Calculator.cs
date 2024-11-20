9cdb3e116e4103714d780ad815f3fb83ea958a1f
P4r4cosm <f1nigan567@gmail.com>
2024-11-20 19:47:59 +0300

    add class Operation, Function and Calculator

---
+++ Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        static double DoOperation(double a, double b, Operation operation)
+        {
+            switch (operation.MathOperation)
+            {
+                case "+": return a+b;
+                case "-": return a-b;
+                case "*": return a*b;
+                case "/": return a/b;
+                case "^": return Math.Pow(a, b);
+                case "%": return a%b;
+                default: return 0;
+            }
+        }
+        static double DoFunction(double a, Function function)
+        {
+           switch (function.Func)
+            {
+                case "sin":return Math.Sin(a);
+                case "cos": return Math.Cos(a);
+                default: return 0;
+            }
+        }
+    }
+}
---
+++ Function.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Function
+    {
+        public string Func { get; private set; }
+        public Function(string func)
+        {
+            Func = func;
+        }
+    }
+}
